f40461368dcd7e94ced7cc6ef76be5fce926e5d3
Aditya Saini <adityasaini372005@gmail.com>
2026-06-09 10:04:41 +0530

    Add files via upload

---
+++ FactRecursion.java
@@ -0,0 +1,27 @@
+
+
+public class FactRecursion {
+ /*    static void factorial(int num, int fact){
+        if(num==0){
+            System.out.println(fact);
+            return ;
+        }
+         fact=fact*num;
+         factorial(num-1,fact);// tail
+    }
+    public static void main(String[] args) {
+        int n=5;
+        factorial(n,1);
+    }
+  }*/
+ static int factorial(int num){
+    if(num==0){
+        return 1;
+    }
+    
+     public static void main(String[] args) {
+        factorial(5);
+     }
+
+
+}
